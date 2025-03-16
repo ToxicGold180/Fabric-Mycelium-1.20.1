a3bc134adeb87bc65aa86718f4aee75d9a4524ea
ToxicGold180 <zach.otto.schmalz@gmail.com>
2025-03-15 22:33:40 -0600

    Initial Commit

---
+++ src/main/java/net/toxicgold180/myceliumadditions/MyceliumAdditions.java
@@ -0,0 +1,16 @@
+package net.toxicgold180.myceliumadditions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyceliumAdditions implements ModInitializer {
+	public static final String MOD_ID = "myceliumadditions";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/toxicgold180/myceliumadditions/MyceliumAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.toxicgold180.myceliumadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyceliumAdditionsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
